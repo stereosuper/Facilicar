4257d32ce48c60d3261ad296bfdc049adebed183
adrienlementheour <adrien.le.mentheour@gmail.com>
2016-01-12 11:07:06 +0100

    :art: Slider détail véhicule

--- js/script.js
+++ js/script.js
@@ -284,6 +284,13 @@ $(function(){
 
 	// Slider detail
 	if($("body").hasClass("slider-detail")){
+		var status = $('.paging-info');
+		var slickElement = $('.slider-for');
+		slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+			//currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+			var i = (currentSlide ? currentSlide : 0) + 1;
+			status.text(i + '/' + slick.slideCount);
+		});
 		 $('.slider-for').slick({
 			slidesToShow: 1,
 			slidesToScroll: 1,
